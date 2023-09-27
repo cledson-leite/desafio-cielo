68e4429b66d20ed0af92e140d9ec4f5fa1b0fa11
Cledson Leite <csbetsonline@gmail.com>
2023-09-26 21:33:47 -0300

    feat: implement responsiveness in the footer component

--- src/view/components/Footer/styles.ts
+++ src/view/components/Footer/styles.ts
@@ -12,19 +12,35 @@ export const Container = styled.div`
 export const Text = styled.p`
     font-size: 1.25rem;
     font-weight: 400;
+
+    @media (max-width: 512px) {
+         font-size: 1rem;
+    }
 `
 export const Name = styled.h2`
     font-size: 1.5rem;
     font-weight: 700;
+    
+    @media (max-width: 512px) {
+         font-size: 1rem;
+         text-align: center;
+    }
 `
 export const Action = styled.span`
     display: flex;
     justify-content: space-between;
     align-items: center;
     width: 5rem;
+    @media (max-width: 512px) {
+        width: 2.25rem;
+    }
 `
 export const Link = styled.a`
     text-decoration: none;
     font-size: 2rem;
     color: ${({theme}) => theme.text};
+
+    @media (max-width: 512px) {
+        font-size: 1rem;
+    }
 `
